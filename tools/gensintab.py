729ac7460176af95c3d6c29483de81451e39bdd8
U-KUUSNEPA\Sami <skyostil@kuusnepa.(none)>
2009-07-31 03:26:19 +0300

    Sine table generator

---
+++ tools/gensintab.py
@@ -0,0 +1,17 @@
+import math
+
+size = 256
+shift = 16
+
+print "const int sintab[%d] =" % size
+print "{"
+print "   ",
+
+for i in range(size):
+    print "%d," % int(float(1 << shift) * math.sin(2 * math.pi * i / float(size))),
+    if not i % 8: 
+        print
+        print "   ",
+
+print
+print "};"
